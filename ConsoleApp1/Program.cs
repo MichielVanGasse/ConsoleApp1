acc8260a2958826d4006381552f58585d67804e1
George Nistor <georgnistor@outlook.de>
2020-02-01 02:16:53 +0200

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x, y;
+            x = 10;
+            y = 3;
+            float result = (float) x /y ;
+            Console.WriteLine($"result={result}");
+            */
+            string name = "George";
+            ChangeName(ref name);
+            
+            Console.WriteLine($"name= {name}");
+
+        }
+
+        static void ChangeName(ref string x)
+        {
+            x = "Nistor";
+        }
+    }
+}
